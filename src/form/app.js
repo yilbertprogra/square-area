7c68958aab8eec05901445ad281eb7fe6f61c993
Yilbert Leonardo Luna Escobar <yilbert.luna.e@gmail.com>
2022-06-21 10:35:50 -0400

    new commit

--- src/form/app.js
+++ src/form/app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-// app.get('/', (req, res) => {
-//     res.send('got a PUT request at / user');
-// })
-
-
+app.get('', (req, res) => {
+    res.send(__dirname + 'index.html');
+})
 
 app.listen(port, () => {
     console.log(`el server esta corriendo en http://localhost:${port}`)
